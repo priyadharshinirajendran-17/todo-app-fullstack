3120fcdcbac7503de7b68fbec7373f9691564e6a
Priya <priyadharshinirajendran1701@gmail.com>
2025-12-01 13:27:34 +0530

    Added backend folder

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Schema
+const TodoSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+// Routes
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+  const todo = await Todo.create(req.body);
+  res.json(todo);
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(todo);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
